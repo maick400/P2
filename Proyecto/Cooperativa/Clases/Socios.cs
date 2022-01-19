b33bb0b1d4f6653bb408e23899924166ac93bc14
vales-alfre <69704088+maick400@users.noreply.github.com>
2022-01-18 23:06:55 -0500

    cracion socios vrn2

--- Proyecto/Cooperativa/Clases/Socios.cs
+++ Proyecto/Cooperativa/Clases/Socios.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
 
 namespace Proyecto.Cooperativa.Clases
 {
+    /*************atributo******************/
     class Socios
     {
+       public int id {get; set;}
+        Cooperativa cooperativa { get; set; }
+        string nombre { get; set; }
+        string apellido { get; set; }
+        int cedula { get; set; }
+        string email { get; set; }
+        string direccion { get; set; }
+        string fechainicio { get; set; }
+
+        /*funciones*/
+        public DataTable listarsocios()
+        {
+            return new DataTable();
+        }
+
+        /****metodos****/
+        public void Agregarsocios()
+        {
+
+        }
+
+        public void Modificar()
+        {
+
+        }
+
+        public void Eliminar()
+        {
+
+        }
     }
+
+   
+
+
+
 }
